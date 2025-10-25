5e7636171672635e4d23b12a1890a394b1e8f50f
manish-env <manishbsn188@gmail.com>
2025-10-25 22:33:08 +0530

    ok

--- .wrangler/tmp/bundle-9uFqoa/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-9uFqoa/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\manis\\OneDrive\\Documents\\WORK\\WORK_November\\RJS_Preorder\\.wrangler\\tmp\\bundle-FpWrlG\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\manis\\OneDrive\\Documents\\WORK\\WORK_November\\RJS_Preorder\\.wrangler\\tmp\\bundle-9uFqoa\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\manis\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\manis\\OneDrive\\Documents\\WORK\\WORK_November\\RJS_Preorder\\.wrangler\\tmp\\bundle-FpWrlG\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\manis\\OneDrive\\Documents\\WORK\\WORK_November\\RJS_Preorder\\.wrangler\\tmp\\bundle-9uFqoa\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\manis\\OneDrive\\Documents\\WORK\\WORK_November\\RJS_Preorder\\.wrangler\\tmp\\bundle-FpWrlG\\middleware-insertion-facade.js";
+export * from "C:\\Users\\manis\\OneDrive\\Documents\\WORK\\WORK_November\\RJS_Preorder\\.wrangler\\tmp\\bundle-9uFqoa\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
